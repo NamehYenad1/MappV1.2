9f070839a294f58b8f7075cf55bfc0c06d580c75
Heman <hemandaney@yahoo.com>
2017-08-02 01:32:43 +0800

    fixed saving private key hashed version to database Added shared preference after the user signs up to use in the encryptions and decryptions

--- app/src/main/java/com/mappassignment/android/mapp/SecondAutentication.java
+++ app/src/main/java/com/mappassignment/android/mapp/SecondAutentication.java
@@ -2,12 +2,44 @@ package com.mappassignment.android.mapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SecondAutentication extends AppCompatActivity {
-
+    private Button mEnter;
+    private EditText mKeyInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mEnter=(Button) findViewById(R.id.EnterKey);
+        mKeyInput=(EditText) findViewById(R.id.KeyInput);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_autentication);
     }
+
+    public String Hash(String PrivateKey, String   salt){
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt.getBytes("UTF-8"));
+            byte[] bytes = md.digest(PrivateKey.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++){
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+
+
 }
